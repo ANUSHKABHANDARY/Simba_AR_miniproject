e969f902b40241ef7eb9b2b6e9416e52a1f2d849
Joel Devasia <joeldevasia1962002@gmail.com>
2022-03-05 16:02:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/simba/argenesis/MainActivity.java
@@ -0,0 +1,69 @@
+package com.simba.argenesis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.assets.RenderableSource;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private static final String GLTF_ASSET =
+            "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor  = hitResult.createAnchor();
+
+            ModelRenderable.builder()
+                    .setSource(this, RenderableSource.builder().setSource(
+                            this,
+//                            Uri.parse(GLTF_ASSET),
+                            Uri.parse("nasa.glb"),
+                            RenderableSource.SourceType.GLB)
+                            .setScale(0.001f)  // Scale the original model to 50%.
+                            .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                            .build())
+//                    .setRegistryId(GLTF_ASSET)
+                    .setRegistryId("nasa.glb")
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+                    .exceptionally(
+                            throwable -> {
+                                Toast toast =
+                                        Toast.makeText(this, "Unable to load renderable " +
+                                                GLTF_ASSET, Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                toast.show();
+                                return null;
+                            });
+
+        });
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 0, 1f), 180));
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        transformableNode.select();
+    }
+}
