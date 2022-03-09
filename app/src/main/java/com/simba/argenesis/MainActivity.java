787a3bd7f77046f36e7e9ccf52fecb4987372aa0
Joel Devasia <joeldevasia1962002@gmail.com>
2022-03-09 22:38:19 +0530

    Added Audible Description via Play Button Added a Delete Button to clear the AR Scene

--- app/src/main/java/com/simba/argenesis/MainActivity.java
+++ app/src/main/java/com/simba/argenesis/MainActivity.java
@@ -2,24 +2,14 @@ package com.simba.argenesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Configuration;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.Size;
 import android.view.Gravity;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
-import com.google.ar.core.CameraConfig;
-import com.google.ar.core.CameraConfigFilter;
-import com.google.ar.core.Config;
-import com.google.ar.core.Session;
-import com.google.ar.core.exceptions.UnavailableApkTooOldException;
-import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
-import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
-import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.assets.RenderableSource;
 import com.google.ar.sceneform.math.Quaternion;
@@ -28,104 +18,19 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
-import java.util.EnumSet;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     private ArFragment arFragment;
+    Button clearArView;
     private static final String GLTF_ASSET =
             "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf";
 
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//
-//        // In some cases modifying newConfig leads to unexpected behavior,
-//        // so it's better to edit new instance.
-//        Configuration configuration = new Configuration(newConfig);
-//        SystemUtils.adjustFontScale(getApplicationContext(), configuration);
-//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Configuration configuration = getResources().getConfiguration();
-//        configuration.fontScale = (float) 1; //0.85 small size, 1 normal size, 1,15 big etc
-//        DisplayMetrics metrics = new DisplayMetrics();
-//        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-//        metrics.scaledDensity = configuration.fontScale * metrics.density;
-//        configuration.densityDpi = (int) getResources().getDisplayMetrics().xdpi;
-//        getBaseContext().getResources().updateConfiguration(configuration, metrics);
-
         setContentView(R.layout.activity_main);
+
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
-
-//        Session session = null;
-//        try {
-//            session = new Session(this);
-//        } catch (UnavailableArcoreNotInstalledException | UnavailableApkTooOldException | UnavailableSdkTooOldException | UnavailableDeviceNotCompatibleException e) {
-//            e.printStackTrace();
-//        }
-//
-//// ...
-//
-//        Size selectedSize = new Size(0, 0);
-//        CameraConfig selectedCameraConfig = null;
-//
-//        //assert session != null;
-//        int flag = 0;
-//        CameraConfigFilter filter = new CameraConfigFilter(session);
-//        List<CameraConfig> cameraConfigsList = session.getSupportedCameraConfigs(filter);
-//        for (CameraConfig currentCameraConfig : cameraConfigsList) {
-//            Size cpuImageSize = currentCameraConfig.getImageSize();
-//            Size gpuTextureSize = currentCameraConfig.getTextureSize();
-//            Log.d("TAG", "CurrentCameraConfig CPU image size:" + cpuImageSize + " GPU texture size:" + gpuTextureSize);
-//
-//            // Adapt this check to your needs
-//            if (gpuTextureSize.getWidth() > selectedSize.getWidth()) {
-//                if (flag == 0){
-//                    flag++;
-//                    selectedSize = gpuTextureSize;
-//                    selectedCameraConfig = currentCameraConfig;
-//                }
-//
-//            }
-//        }
-//
-//        Log.d("TAG", "Selected CameraConfig CPU image size:" + selectedCameraConfig.getImageSize() + " GPU texture size:" + selectedCameraConfig.getTextureSize());
-//        session.setCameraConfig(selectedCameraConfig);
-
-// ...
-
-// Don't forget to configure the session afterwards
-        //session.configure(config);
-
-//        // Create a camera config filter for the session.
-//        Session session = null;
-//        try {
-//            session = new Session(arFragment.requireActivity());
-//        } catch (UnavailableArcoreNotInstalledException | UnavailableApkTooOldException | UnavailableSdkTooOldException | UnavailableDeviceNotCompatibleException e) {
-//            e.printStackTrace();
-//        }
-//        assert session != null;
-//        CameraConfigFilter filter = new CameraConfigFilter(session);
-//
-//// Return only camera configs that target 30 fps camera capture frame rate.
-//        filter.setTargetFps(EnumSet.of(CameraConfig.TargetFps.TARGET_FPS_30));
-//
-//// Return only camera configs that will not use the depth sensor.
-//        filter.setDepthSensorUsage(EnumSet.of(CameraConfig.DepthSensorUsage.DO_NOT_USE));
-//
-//// Get list of configs that match filter settings.
-//// In this case, this list is guaranteed to contain at least one element,
-//// because both TargetFps.TARGET_FPS_30 and DepthSensorUsage.DO_NOT_USE
-//// are supported on all ARCore supported devices.
-//        List<CameraConfig> cameraConfigList = session.getSupportedCameraConfigs(filter);
-//
-//// Use element 0 from the list of returned camera configs. This is because
-//// it contains the camera config that best matches the specified filter
-//// settings.
-//        session.setCameraConfig(cameraConfigList.get(0));
 
         assert arFragment != null;
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
@@ -154,6 +59,13 @@ public class MainActivity extends AppCompatActivity {
                                 return null;
                             });
 
+        });
+
+        clearArView = findViewById(R.id.clear_ar_viewButton);
+        clearArView.setOnClickListener(view -> {
+            Intent i = new Intent(this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            this.startActivity(i);
         });
 
     }
--- app/src/main/java/com/simba/argenesis/Model_Info.java
+++ app/src/main/java/com/simba/argenesis/Model_Info.java
@@ -1,63 +1,134 @@
 package com.simba.argenesis;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import java.util.Objects;
+import java.io.StringReader;
+import java.util.Locale;
+
 
 public class Model_Info extends AppCompatActivity {
 
     View viewAR;
     ImageButton backButton, favButton, moreButton;
     Toast mToast = null;
+    TextToSpeech TTS;
+
+    final boolean[] sound = {false};
+
+    Button SoundButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Objects.requireNonNull(this.getSupportActionBar()).hide();
         setContentView(R.layout.activity_model_info);
+
 
         viewAR = findViewById(R.id.View_AR_Model_Button);
         viewAR.setOnClickListener(view -> {
             Toast.makeText(Model_Info.this, "View Model", Toast.LENGTH_SHORT).show();
-            Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         });
         backButton = findViewById(R.id.Back_Button);
-        favButton  = findViewById(R.id.Favourite_Button);
+        favButton = findViewById(R.id.Favourite_Button);
         moreButton = findViewById(R.id.More_Button);
 
         backButton.setOnClickListener(view -> {
-//                Toast.makeText(Model_Info.this, "Back Button", Toast.LENGTH_SHORT).show();
             showAToast("Back Button");
         });
 
         favButton.setOnClickListener(view -> {
-//                Toast.makeText(Model_Info.this, "Favourite Button", Toast.LENGTH_SHORT).show();
             showAToast("Favourite Button");
         });
 
         moreButton.setOnClickListener(view -> {
-//                Toast.makeText(Model_Info.this, "More Button", Toast.LENGTH_SHORT).show();
             showAToast("More Button");
         });
 
+        TTS = new TextToSpeech(this, status -> {
+            if (status == TextToSpeech.SUCCESS) {
+                int result = TTS.setLanguage(new Locale("en", "IN"));
+
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    showAToast("Language Data Missing or Language Not Supported");
+                }
+            } else {
+                showAToast("Initialization Failed");
+            }
+            TTS.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onStart(String s) {
+
+                }
+
+                @Override
+                public void onDone(String s) {
+                    runOnUiThread(() -> {
+                        SoundButton.setBackgroundResource(R.drawable.ic_stop_audio);
+                    });
+                }
+
+                @Override
+                public void onError(String s) {
+
+                }
+            });
+        });
+
+
+        SoundButton = findViewById(R.id.Sound_BT);
+        SoundButton.setOnClickListener(view -> {
+            if (!sound[0]) {
+                SoundButton.setBackgroundResource(R.drawable.ic_play_audio);
+                sound[0] = true;
+                speak();
+            } else {
+                SoundButton.setBackgroundResource(R.drawable.ic_stop_audio);
+                sound[0] = false;
+                TTS.stop();
+            }
+        });
+
     }
-    public void showAToast (String message){
+
+//    public void setUtterance(UtteranceProgressListener utterance) {
+//
+//    }
+
+
+    private void speak() {
+        String text = getString(R.string.earth_description);
+        SoundButton = findViewById(R.id.Sound_BT);
+
+        TTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, "");
+    }
+
+
+    public void showAToast(String message) {
 
         if (mToast != null) {
             mToast.cancel();
         }
         mToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
         mToast.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (TTS != null) {
+            TTS.stop();
+            TTS.shutdown();
+        }
+
+        super.onDestroy();
     }
 }
