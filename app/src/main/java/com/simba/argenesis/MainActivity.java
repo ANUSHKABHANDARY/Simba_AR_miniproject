6c31848f568259d10d1bbc3ac758618a7909cd91
Joel Devasia <joeldevasia1962002@gmail.com>
2022-03-15 01:33:55 +0530

    Now Model is Downloaded from Firebase Storage on Runtime. Gave Rounded Corners to AR Fragment and resized to 9:16 Ratio.

--- app/src/main/java/com/simba/argenesis/MainActivity.java
+++ app/src/main/java/com/simba/argenesis/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Gravity;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.assets.RenderableSource;
@@ -17,6 +18,13 @@ import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,32 +40,49 @@ public class MainActivity extends AppCompatActivity {
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
+        FirebaseApp.initializeApp(this);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference modelRef = storage.getReference().child("Earth.glb");
+
+        try {
+            File file = File.createTempFile("Earth", "glb");
+            modelRef.getFile(file).addOnSuccessListener(taskSnapshot -> buildModel(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         assert arFragment != null;
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
-            Anchor anchor  = hitResult.createAnchor();
 
-            ModelRenderable.builder()
-                    .setSource(this, RenderableSource.builder().setSource(
-                            this,
-//                            Uri.parse(GLTF_ASSET),
-                            Uri.parse("nasa.glb"),
-                            RenderableSource.SourceType.GLB)
-                            .setScale(0.001f)  // Scale the original model to 50%.
-                            .setRecenterMode(RenderableSource.RecenterMode.ROOT)
-                            .build())
-//                    .setRegistryId(GLTF_ASSET)
-                    .setRegistryId("nasa.glb")
-                    .build()
-                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
-                    .exceptionally(
-                            throwable -> {
-                                Toast toast =
-                                        Toast.makeText(this, "Unable to load renderable " +
-                                                GLTF_ASSET, Toast.LENGTH_LONG);
-                                toast.setGravity(Gravity.CENTER, 0, 0);
-                                toast.show();
-                                return null;
-                            });
+            Anchor anchor  = hitResult.createAnchor();
+            addModelToScene(anchor, renderable);
+//            AnchorNode anchorNode  = new AnchorNode(hitResult.createAnchor());
+//            anchorNode.setRenderable(renderable);
+//            arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+//            ModelRenderable.builder()
+//                    .setSource(this, RenderableSource.builder().setSource(
+//                            this,
+////                            Uri.parse(GLTF_ASSET),
+//                            Uri.parse("nasa.glb"),
+//                            RenderableSource.SourceType.GLB)
+//                            .setScale(0.001f)  // Scale the original model to 50%.
+//                            .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+//                            .build())
+////                    .setRegistryId(GLTF_ASSET)
+//                    .setRegistryId("nasa.glb")
+//                    .build()
+//                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+//                    .exceptionally(
+//                            throwable -> {
+//                                Toast toast =
+//                                        Toast.makeText(this, "Unable to load renderable " +
+//                                                GLTF_ASSET, Toast.LENGTH_LONG);
+//                                toast.setGravity(Gravity.CENTER, 0, 0);
+//                                toast.show();
+//                                return null;
+//                            });
 
         });
 
@@ -70,10 +95,47 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private ModelRenderable renderable;
+
+
+    private void buildModel(File file) {
+        RenderableSource renderableSource = RenderableSource
+                .builder()
+                .setSource(this, Uri.parse(file.getPath()), RenderableSource.SourceType.GLB)
+                .setScale(0.001f)  // Scale the original model to 50%.
+                .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                .build();
+
+        ModelRenderable
+                .builder()
+                .setSource(this, renderableSource)
+//                        RenderableSource.SourceType.GLB)
+//                        .setScale(0.001f)  // Scale the original model to 50%.
+//                        .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+//                        .build())
+////                    .setRegistryId(GLTF_ASSET)
+                .setRegistryId(file.getPath())
+                .build()
+                .thenAccept(modelRenderable -> {
+                    Toast.makeText(this, "Model Built", Toast.LENGTH_SHORT).show();
+                    renderable = modelRenderable;
+//                    addModelToScene(anchor, modelRenderable);
+                });
+//                .exceptionally(
+//                        throwable -> {
+//                            Toast toast =
+//                                    Toast.makeText(this, "Unable to load renderable " +
+//                                            GLTF_ASSET, Toast.LENGTH_LONG);
+//                            toast.setGravity(Gravity.CENTER, 0, 0);
+//                            toast.show();
+//                            return null;
+//                        });
+    }
+
     private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
         AnchorNode anchorNode = new AnchorNode(anchor);
         TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
-        transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0.0f, 0.0f, 0.0f), 180));
+        transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0.0f, 270.0f, 0.0f), 180));
         transformableNode.setParent(anchorNode);
         transformableNode.setRenderable(modelRenderable);
         arFragment.getArSceneView().getScene().addChild(anchorNode);
